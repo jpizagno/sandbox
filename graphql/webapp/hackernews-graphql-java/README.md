c8210425d729eaa77448020101545f38c304034e
jpizagno <jpizagno@gmail.com>
2018-10-15 21:44:26 +0200

    first GraphQL query works in browser. example includes GraphiQL

--- graphql/webapp/hackernews-graphql-java/README.md
+++ graphql/webapp/hackernews-graphql-java/README.md
@@ -1,4 +1,26 @@
+
+#### reference
 tutorial from
 https://www.howtographql.com/graphql-java/1-getting-started/
 
-You can run the app just by executing mvn jetty:run in the directory where pom.xml is located, and Jetty will start on port 8080.
+#### run
+You can run the app just by executing the run script:
+```
+shell% ./run.sh
+```
+
+#### issues
+This project only seems to run with old version of graphql* in the pom.xml.
+
+#### testing
+in-browser IDE for exploring GraphQL:
+http://localhost:9999/
+
+enter in left-panel of browser:
+```
+{
+  allLinks {
+    url
+  }
+}
+```
--- graphql/webapp/hackernews-graphql-java/src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ graphql/webapp/hackernews-graphql-java/src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -2,13 +2,16 @@ package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
-import graphql.servlet.SimpleGraphQLServlet;
 
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
 
 /**
  * This class with import graphql.servlet.SimpleGraphQLServlet; seems to work with graphql-java-servlet and lower.
  *
  * http://localhost:8080/graphql
+ *
+ * http://localhost:8080/graphql?query={allLinks{url}}
  *
  */
 
@@ -16,9 +19,20 @@ import graphql.servlet.SimpleGraphQLServlet;
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
     public GraphQLEndpoint() {
-        super(SchemaParser.newParser()
-                .file("schema.graphqls") //parse the schema file created earlier
+        super(buildSchema());
+    }
+
+    /**
+     * register the resolver properly when generating the schema
+     *
+     * @return
+     */
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
                 .build()
-                .makeExecutableSchema());
+                .makeExecutableSchema();
     }
 }

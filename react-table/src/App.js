af2812a8eeb431c462c6cec65c0f882866bbda14
jpizagno <jpizagno@gmail.com>
2020-02-19 19:30:04 +0100

    Working version, table changes after componentmounted

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -4,9 +4,6 @@ import { useTable, useFilters, useGlobalFilter } from 'react-table'
 // A great library for fuzzy filtering/sorting items
 import matchSorter from 'match-sorter'
 
-import makeData from './makeData';
-
-
 const Styles = styled.div`
   padding: 1rem;
 
@@ -112,94 +109,6 @@ function SelectColumnFilter({
   )
 }
 
-// This is a custom filter UI that uses a
-// slider to set the filter value between a column's
-// min and max values
-function SliderColumnFilter({
-  column: { filterValue, setFilter, preFilteredRows, id },
-}) {
-  // Calculate the min and max
-  // using the preFilteredRows
-
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    preFilteredRows.forEach(row => {
-      min = Math.min(row.values[id], min)
-      max = Math.max(row.values[id], max)
-    })
-    return [min, max]
-  }, [id, preFilteredRows])
-
-  return (
-    <>
-      <input
-        type="range"
-        min={min}
-        max={max}
-        value={filterValue || min}
-        onChange={e => {
-          setFilter(parseInt(e.target.value, 10))
-        }}
-      />
-      <button onClick={() => setFilter(undefined)}>Off</button>
-    </>
-  )
-}
-
-// This is a custom UI for our 'between' or number range
-// filter. It uses two number boxes and filters rows to
-// ones that have values between the two
-function NumberRangeColumnFilter({
-  column: { filterValue = [], preFilteredRows, setFilter, id },
-}) {
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    preFilteredRows.forEach(row => {
-      min = Math.min(row.values[id], min)
-      max = Math.max(row.values[id], max)
-    })
-    return [min, max]
-  }, [id, preFilteredRows])
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-      }}
-    >
-      <input
-        value={filterValue[0] || ''}
-        type="number"
-        onChange={e => {
-          const val = e.target.value
-          setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
-        }}
-        placeholder={`Min (${min})`}
-        style={{
-          width: '70px',
-          marginRight: '0.5rem',
-        }}
-      />
-      to
-      <input
-        value={filterValue[1] || ''}
-        type="number"
-        onChange={e => {
-          const val = e.target.value
-          setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
-        }}
-        placeholder={`Max (${max})`}
-        style={{
-          width: '70px',
-          marginLeft: '0.5rem',
-        }}
-      />
-    </div>
-  )
-}
-
 function fuzzyTextFilterFn(rows, id, filterValue) {
   return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
 }
@@ -244,9 +153,6 @@ function Table({ columns, data }) {
     rows,
     prepareRow,
     state,
-    flatColumns,
-    preGlobalFilteredRows,
-    setGlobalFilter,
   } = useTable(
     {
       columns,
@@ -335,8 +241,6 @@ filterGreaterThan.autoRemove = val => typeof val !== 'number'
 
 function App(props)  {
 
-  console.log("props.data = " + props.data);
-
   const columns = React.useMemo(
     () => [
       {
@@ -346,11 +250,11 @@ function App(props)  {
       },
       {
         Header: 'Title',
-        accessor: 'link',
+        accessor: 'title',
         disableFilters: true, 
         Cell: ({ row }) => {
           return (
-            <a href={row.original.link}>{row.original.linkname}</a> 
+            <a href={row.original.url}>{row.original.title}</a> 
           );
         }
       },
@@ -364,11 +268,9 @@ function App(props)  {
     []
   )
 
-  const data = React.useMemo(() => props.data, []) // makeData(100000), [])
-
   return (
     <Styles>
-      <Table columns={columns} data={data} />
+      <Table columns={columns} data={props.data} />
     </Styles>
   )
 }
--- react-table/src/index.js
+++ react-table/src/index.js
@@ -10,23 +10,48 @@ class StartPage extends Component {
     super();
     this.state = { 
       data: [
-        {"source":  "cnn", "link": "http://www.cnn.com", "linkname": "title1", "topic": "topic1"}
-        , {"source":  "fox", "link": "http://www.foxnews.com", "linkname": "title2", "topic": "topic1"}
-        , {"source":  "fox", "link": "http://www.foxnews.com", "linkname": "title3", "topic": "topic2"}
-        , {"source":  "huffpo", "link": "http://www.huggingtonpost.com", "linkname": "title4", "topic": "topic2"}
+        {"source":  "cnn", "url": "http://www.cnn.com", "title": "title1", "topic": "topic1"}
+        , {"source":  "fox", "url": "http://www.foxnews.com", "title": "title2", "topic": "topic1"}
+        , {"source":  "fox", "url": "http://www.foxnews.com", "title": "title3", "topic": "topic2"}
+        , {"source":  "huffpo", "url": "http://www.huggingtonpost.com", "title": "title4", "topic": "topic2"}
       ]
     };
+  }
 
+  componentWillMount() {
+    this.loadTopicMatrix();
+  }
 
+  loadTopicMatrix() {
+
+    let url = 'http://127.0.0.1:8093/mediamatrix';
+    fetch(url, {
+      credentials: 'same-origin',
+      method:'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }})
+    .then(response => response.json())
+    .then(mediamatrix => {
+      // Mediamatrix will have the same format as the data in the constructor()
+      this.setState({data: mediamatrix});
+
+      // have to create a new object for the change of state to register in React-Table
+      // https://github.com/tannerlinsley/react-table/issues/175
+      let newData = [...mediamatrix];
+
+      this.setState({data: newData });
+
+    });
   }
 
   render() {
     return (
-      <div classname="container">
+      <div className="container">
         <h1>Hello World!</h1>
         <p>I just created my first React App</p>
         <App data={this.state.data}></App>
-      
       </div>
     );
   }
